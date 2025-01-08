84bf569652bf1e9119713e4445d74ef4f268ee62
Natesworks <contact@natesworks.com>
2025-01-08 14:44:39 +0100

    xDim dumb

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       <body className={`${inter.className} flex flex-col min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
+          <main className="flex-grow container mx-auto px-4 py-8 mt-14">
+            {children}
+          </main>
           <Script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3444011043022708"
