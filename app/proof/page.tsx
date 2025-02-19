f0a49598e3ea9372b2f45b9acc3d7ea41baff9cd
Natesworks <contact@natesworks.com>
2025-02-19 15:54:15 +0100

    Delete app/proof/page.tsx

--- app/proof/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-gray-800">I am @ntswrk on Telegram</h1>
-    </div>
-  )
-}
-
