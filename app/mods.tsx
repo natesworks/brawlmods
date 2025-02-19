6998a4698a290b955dee9d5bffdd147643827772
Natesworks <contact@natesworks.com>
2025-02-19 12:51:28 +0100

    Update mods.tsx

--- app/mods.tsx
+++ app/mods.tsx
@@ -277,10 +277,6 @@ We are trying to add new brawlers and features as fast as possible. Sometimes it
                 version: "V45",
                 downloadLink: "https://dl.natesworks.com/brawlstars/servers/bsds/com.projectbsds.v45225.apk"
             },
-            {
-                version: "V22",
-                downloadLink: "https://dl.natesworks.com/brawlstars/clients/v22/v22.apk"
-            }
         ]
     },
     {
